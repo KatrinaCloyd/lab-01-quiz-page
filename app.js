f7bdc77f691c1a025e989498087980d921a6da66
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-11 15:14:29 -0800

    create all prompts and confirm connection

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const quizBtn = document.getElementById('takeQuizBtn');
 quizBtn.addEventListener('click', () => {
     alert('Welcome to the Sea Otter Quizzz!');
 
-    confirm('Do you really feel ready for this?');
+    const quizConfirm = confirm('Do you really feel ready for this?');
+    if (!quizConfirm) return;
 
-})
+    const firstName = prompt('What is your first name?');
+    console.log(firstName);
+
+    const secondName = prompt('What is your last name?');
+    console.log(secondName);
+
+    const ansOne = prompt('Do sea otters eat sea urchins?');
+    console.log(ansOne);
+
+    const ansTwo = prompt('Do otters hold hands to stay near one another?');
+    console.log(ansTwo);
+
+    const ansThree = prompt('Are sea otter pups able to feed themselves?');
+    console.log(ansThree);
+
+});
