299768b48f653c0347d8ee996c674b65e7af77bb
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-11 16:59:27 -0800

    add testing

--- README.md
+++ README.md
@@ -7,18 +7,26 @@ Make A Plan:
 
 -JS 
     -event listner to button 
+                -console.log to check connection
         -should confirm "ready for this quiz?"
+                -console.log to check connection
             -close if no
         -should prompt 'first name' and save it 
+                -console.log to check connection
         -should prompt 'last name' and save it 
+                -console.log to check connection
         -create const for correct answers
+                -console.log to check connection
         -Should ask yes or no question #1 
+                -console.log to check connection
             - check if Y 
             - ++ correct answers if correct 
         -Should ask yes or no question #2 
+                -console.log to check connection
             - check if NOT Y 
             - ++ correct answers if correct 
         -Should ask yes or no question #3
+                -console.log to check connection
             - check if NOT Y 
             - ++ correct answers if correct 
     -change text content to show user name and quiz results
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,38 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { countsAsYes } from '../utils.js';
+import { percentage } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
+test('should return true when a word starting with a Y is given', (expect) => {
     const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    const actual = countsAsYes('yelp');
     expect.equal(actual, expected);
 });
+
+test('should return false when a word that does not start with a Y is given', (expect) => {
+    const expected = false;
+    const actual = countsAsYes('tornado');
+    expect.equal(actual, expected);
+});
+
+
+test('should return 67 when given 2', (expect) => {
+    const expected = 67;
+    const actual = percentage(2, 3);
+    expect.equal(actual, expected);
+});
+
+test('should return 100 when given 3', (expect) => {
+    const expected = 100;
+    const actual = percentage(3, 3);
+    expect.equal(actual, expected);
+});
+
+test('should return 33 when given 1', (expect) => {
+    const expected = 33;
+    const actual = percentage(1, 3);
+    expect.equal(actual, expected);
+});
+
